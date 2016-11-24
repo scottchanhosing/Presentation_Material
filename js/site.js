6197191ce1db55ef3aed78df5577c82feefcb6a6
scottchanhosing <Scott.Chan@coherent.com.hk>
2016-11-24 18:30:59 +0800

    big updated
    for final presentation

--- js/site.js
+++ js/site.js
@@ -1,5 +1,6 @@
 ﻿// Write your Javascript code.
 eventdata = []
+var selected_day
 
 $.ajax({
     url: "data.csv",
@@ -15,125 +16,40 @@ $.ajax({
 });
 var app = angular.module("EventListApp", []);
 app.controller("EventCtrl", function ($scope) {
-    /*
-    $scope.events = [
-        {
-            Name: "Financial Dialogue Day",
-            StartDate: "13/11/2017",
-            EndDate: "18/11/2016",
-            StartTime: "18:30",
-            EndTime: "20:30",
-            Venue: "CUHK Campus",
-            Target: "Members of The China Trade Society",
-            Deadline: "18/10/2016 12:00",
-            Link: "https://goo.gl/forms/ZTiFUZlncRlNo9xA3",
-            Nature: "Talk",
-            Description: "The China Trade Society is holding the Financial Dialogue Day on Oct 13 (Thursday) and Oct 18 (Tuesday) respectively. " +
-                        "A number of sophisticated speakers, covering investment banking, private equity, real estate investment, risk and wealth management and insurance, are going to share their experiences and ideas in the event."+
-                        "By joining the event, you are expected to: "+
-                        "1. Connect to the latest investment banking development and future trend "+
-                        "2. Understand potential financial opportunities in the Chinese market "+
-                        "3. Identify the potential risks and mitigation " +
-                        "4. Equip yourself with mindset and know-how to excel in future financial market "+
-                        "5. Get to know the available internships offered by the firms the speakers are working in."
-        },
-        {
-            Name: "ACCA Hong Kong Business Competition 2016",
-            StartDate: "01/11/2016",
-            EndDate: "31/11/2016",
-            StartTime: "00:00",
-            EndTime: "23:59",
-            Venue: "CUHK Campus",
-            Target: "All CUHK students",
-            Deadline: "19/10/2016 16:00",
-            Link: "http://www.anpasia.com/public/run-form.aspx?Id=46405C427745425843&EventId=45445C417340405843&SessionId=4646584575494A5A43",
-            Nature: "Competition",
-            Description: "SIE Fund and Youth.gov.hk jointly organise the SI (Social Innovation) CEO Competition with the theme 'Tackling Poverty Problems with Social Innovation'."+
-                        "The Competition comprises experiential programmes, talk and the contest, with the objectives of enhancing understanding of 'poverty relief' and 'social innovation', inspiring innovative ideas and actions to tackle social and poverty problems and nurturing capability in social entrepreneurship among students. "+
-                        "Basing on one, or more than one of the sub-themes, namely Care for Elderly, Food Support and Social Inclusion, participating teams should develop an innovative solution to social issue and formulate a business proposal."
-        },
-        {
-            Name: "Treasury Markets Association (TMA) Challenge 2017",
-            StartDate: "01/11/2016",
-            EndDate: "31/01/2017",
-            StartTime: "00:00",
-            EndTime: "23:59",
-            Venue: "CUHK Campus",
-            Target: "All CUHK students",
-            Deadline: "03/11/2016 23:00",
-            Link: " http://www.tma.org.hk/en_newsevents_n1.aspx?NewsId=300",
-            Nature: "Competition",
-            Description: ""
-        },
-        {
-            Name: "Chicago Quantitative Alliance (CQA) Investment Challenge",
-            StartDate: "31/11/2016",
-            EndDate: "31/03/2017",
-            StartTime: "00:00",
-            EndTime: "23:59",
-            Venue: "CUHK Campus",
-            Target: "All CUHK business students",
-            Deadline: "20/11/2016 23:00",
-            Link: "http://www.cqa.org/investment_challenge",
-            Nature: "Competition",
-            Description: ""
-        },
-        {
-            Name: "Microsoft Hong Kong Limited",
-            StartDate: "12/11/2016",
-            EndDate: "12/11/2016",
-            StartTime: "12:30",
-            EndTime: "14:30",
-            Venue: "CUHK Campus",
-            Target: "All CUHK students",
-            Deadline: "12/11/2016 00:00",
-            Link: "https://cpdc.osa.cuhk.edu.hk/event/detail/microsofttalk20161012a",
-            Nature: "RecruitmentTalk",
-            Description: ""
-        },
-        {
-            Name: "Nan Fung Development Limited",
-            StartDate: "17/11/2016",
-            EndDate: "17/11/2016",
-            StartTime: "12:30",
-            EndTime: "12:30",
-            Venue: "CUHK Campus",
-            Target: "All CUHK students",
-            Deadline: "17/11/2016 00:00",
-            Link: "https://cpdc.osa.cuhk.edu.hk/event/detail/nanfungtalk20161017a",
-            Nature: "RecruitmentTalk",
-            Description: ""
-        },
-        {
-            Name: "Societe Generale",
-            StartDate: "18/11/2016",
-            EndDate: "18/11/2016",
-            StartTime: "16:30",
-            EndTime: "18:30",
-            Venue: "CUHK Campus",
-            Target: "All CUHK students",
-            Deadline: "18/11/2016 00:00",
-            Link: "https://cpdc.osa.cuhk.edu.hk/event/detail/sgtalk20161018p",
-            Nature: "RecruitmentTalk",
-            Description: ""
-        }
-    ];
-    */
     $scope.events = eventdata
     $scope.getItem = function getItem(idx, list, key) {
         return list[idx][key];
     };
     $scope.goToDate = function goToDate(year, month, day) {
-        //console.log(year, month, day);
         $('#datepicker').datepicker('update', new Date(parseInt(year), parseInt(month) - 1, parseInt(day)));
         $('#datepicker_input').val(
             $('#datepicker').datepicker('getFormattedDate')
         );
     }
+    $scope.addData = function addData(event) {
+        data = {};
+        start_date = event.StartMonth + '-' + event.StartDate + '-' + event.StartYear;
+        end_date = event.EndMonth + '-' + event.EndDate + '-' + event.EndYear;
+        data[start_date] = [];
+        content = '<span class="cal-event-name">' + '<i class="fa fa-circle ' + event.Nature + '"></i>'
+        content +=  event.Name + '</span>';
+        if (start_date == end_date) {
+            content += '<span class="cal-event-time"><b>Date:</b> ' + start_date + ' to ' + end_date + '<br>'
+        }
+        else {
+            content += '<span class="cal-event-time"><b>Date:</b> ' + start_date + '<br>'
+        }
+        content += '<b>Time:</b> ' + event.StartTime + ' to ' + event.EndTime + '</span>';
+        content += '<span class="cal-event-venue">' + event.Venue + '</span>';
+        content += '<span class="cal-event-detail">' + event.Description + '</span>';
+        content += '<span class="cal-event-deadline"><b>Deadline:</b> ' + event.Deadline + '</span>';
+        //content += '<span class="cal-event-link">Link:     ' + event.Link + '</span>';
+        data[start_date].push({ 'content': content, endDate: end_date, allDay: true })
+        $('#calendar').calendario('setData', data, false);
+    }
 });
 
 $(function () {
-
     // Instantiate MixItUp:
     $('#Container').mixItUp();
     $('#datepicker').datepicker({
@@ -144,27 +60,30 @@ $(function () {
             $('#datepicker').datepicker('getFormattedDate')
         );
     });
-    $('.responsive-calendar').responsiveCalendar({
-        events: {
-            "2016-10-30": { "number": 5, "badgeClass": "badge-warning"},
-            "2016-10-26": { "number": 1, "badgeClass": "badge-warning"},
-        }
-    });
-
     $('.go-to-calendar').click(function () {
-        $('.go-to-calendar').css("display", "none");
-        $('.go-to-event-list').css("display", "block");
-        $('#search-bar').css("display", "none");
-        $('#search-content').css("display", "none");
-        $('#my-event').css('display', 'block');
+        if ($('#search-bar').css("display") == "block") {
+            $('.go-to-event-list').removeClass("active");
+            $('.go-to-calendar').addClass("active");
+            $('#search-bar').css("display", "none");
+            $('#search-content').css("display", "none");
+            $('#my-event').css('display', 'block');
+            if (!($('.navbar-toggle').css('display') == 'none')) {
+                $('.navbar-toggle').click()
+            }        
+        }
     })
 
     $('.go-to-event-list').click(function () {
-        $('.go-to-calendar').css("display", "block");
-        $('.go-to-event-list').css("display", "none");
-        $('#search-bar').css("display", "block");
-        $('#search-content').css("display", "block");
-        $('#my-event').css('display', 'none');
+        if ($('#search-bar').css("display") == "none") {
+            $('.go-to-event-list').addClass("active");
+            $('.go-to-calendar').removeClass("active");
+            $('#search-bar').css("display", "block");
+            $('#search-content').css("display", "block");
+            $('#my-event').css('display', 'none');
+            if (!($('.navbar-toggle').css('display') == 'none')) {
+                $('.navbar-toggle').click()
+            }
+        }
     })
 
 });
@@ -215,3 +134,74 @@ function fblogin() {
         }
     });
 }
+
+
+$(function () {
+
+    function updateMonthYear() {
+        $('#custom-month').html($('#calendar').calendario('getMonthName'));
+        $('#custom-year').html($('#calendar').calendario('getYear'));
+    }
+
+    $(document).on('finish.calendar.calendario', function (e) {
+        $('#custom-month').html($('#calendar').calendario('getMonthName'));
+        $('#custom-year').html($('#calendar').calendario('getYear'));
+        $('#custom-next').on('click', function () {
+            $('#calendar').calendario('gotoNextMonth', updateMonthYear);
+        });
+        $('#custom-prev').on('click', function () {
+            $('#calendar').calendario('gotoPreviousMonth', updateMonthYear);
+        });
+        $('#custom-current').on('click', function () {
+            $('#calendar').calendario('gotoNow', updateMonthYear);
+        });
+    });
+
+    $('#calendar').on('shown.calendar.calendario', function () {
+        $('div.fc-row > div').on('onDayClick.calendario', function (e, dateprop) {
+            $('.fc-calendar .fc-row > div.selected').removeClass('selected');
+            $(e.target).addClass('selected');
+            console.log(dateprop);
+            if (dateprop.data) {
+                showEvents(dateprop.data.html, dateprop);
+            }
+        });
+    });
+
+    var transEndEventNames = {
+        'WebkitTransition': 'webkitTransitionEnd',
+        'MozTransition': 'transitionend',
+        'OTransition': 'oTransitionEnd',
+        'msTransition': 'MSTransitionEnd',
+        'transition': 'transitionend'
+    },
+    transEndEventName = transEndEventNames[Modernizr.prefixed('transition')],
+    $wrapper = $('#custom-inner');
+
+    function showEvents(contentEl, dateprop) {
+        hideEvents();
+        var $events = $('<div id="custom-content-reveal" class="custom-content-reveal"><h4>Events for ' + dateprop.monthname + ' '
+        + dateprop.day + ', ' + dateprop.year + '</h4></div>')
+        $close = $('<span class="custom-content-close"></span>').on('click', hideEvents);
+        $events.append(contentEl.join(''), $close).insertAfter($wrapper);
+        setTimeout(function () {
+            $events.css('top', '0%');
+        }, 25);
+    }
+
+    function hideEvents() {
+        var $events = $('#custom-content-reveal');
+        if ($events.length > 0) {
+            $events.css('top', '100%');
+            Modernizr.csstransitions ? $events.on(transEndEventName, function () { $(this).remove(); }) : $events.remove();
+        }
+    }
+
+    $('#calendar').calendario({
+        caldata: events,
+        displayWeekAbbr: true,
+        events: ['click', 'focus']
+    });
+})
+
+
